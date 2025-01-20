95486203ae931e6128fd55244a531effe86476b8
Sebastian_Acevedo <acevedosebastian455@gmail.com>
2025-01-20 09:00:56 +0100

    js

--- eje18.js
+++ eje18.js
@@ -4,3 +4,11 @@ Imprime en un console log el array resultante.
 
 Puedes usar este array: */
 const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+// Usando un bucle for para recorrer todos los destinos del array y elimina 
+// los elementos que tengan el id 11 y el id 40
+for (let i = placesToTravel.length - 1; i >= 0; i--){
+    if (placesToTravel[i].id === 11 || placesToTravel[i].id === 40) {
+        placesToTravel.splice(i, 1);
+        }
+        }
+        console.log(placesToTravel);
